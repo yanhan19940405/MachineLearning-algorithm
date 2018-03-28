29868af293aa44ee6669e611e9c4483e8fa0f25b
Your Name <you@example.com>
2018-03-28 12:00:48 +0800

    更改logistic梯度下降终止条件

--- logistic/logistic .py
+++ logistic/logistic .py
@@ -26,25 +26,29 @@ if __name__=='__main__':
     w1=[]
     learn=0.01
     w=np.zeros((100,2))
+    w=w*1.00000
     h=np.dot(w,a.T)
 #h=w.T.dot(a)
-    while  n<100:
+    while  n<100 :
         w=w-learn*(np.dot(np.dot(w[n],(a[n,0:2].T))-b[n,2],a[n,0:2]))
         j=np.square((np.dot(w[n],(a[n,0:2].T))-b[n,2]))
         j1 = (1 / 2) * j
         l.append(j1)
         w1.append(w)
-        n=n+1
-    print(l)
-    k=sorted(l)
-    print(k)
-    for v in enumerate(l,0):#此处运用enumerate遍历l得到w数组中最小项和其索引组成的元组
-        if v[1]==k[1]:
-            print(v)
+        print(np.abs(w1[n][0, 0] - w1[n - 1][0, 0]) )
+        if np.abs(w1[n][0, 0] - w1[n - 1][0, 0])==0  or np.abs(w1[n][0, 1] - w1[n - 1][0, 1])==0:
+            if n!=0:
+                print("合适的w",w)
+                print("合适的n", n)
+                w0=w1[n]
+        n = n + 1
+    print("w0",w0)
 
-    print("合适的W值为:",w1[v[0]][0])
+
+    print("合适的W值为:",w0[0])
     #
     x_test=[12.33333343,5.4337643]
     x_test=(x_test-minnum)/(maxnum-minnum)#归一化
     print("x_test",x_test)
-    print("数据X分类属于：","第"+str(classify( w1[v[0]][0],x_test))+"类")
+    print("数据X分类属于：","第"+str(classify( w[0],x_test))+"类")
+
